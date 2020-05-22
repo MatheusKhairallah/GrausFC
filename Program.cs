738f46dcf379cdd41a200451b60c2a06042e2ce1
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-05-22 19:55:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converta 0°F para °C");
+            Console.ReadKey();
+            double FC=0 - 32 / 1.8;
+            Console.WriteLine($"°C={FC}");
+        }
+    }
+}
